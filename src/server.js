bbdb64fac54cf00a470b0c588d93a51e56d8c97f
Franco rossi <rossifranco748@gmail.com>
2024-06-02 18:25:17 -0300

    Actualizando Server

--- src/server.js
+++ src/server.js
@@ -1,27 +1,24 @@
-import express from 'express';
-import { connect } from 'mongoose';
-import cors from 'cors';
-require('dotenv').config();
+import express from "express";
+import { connect } from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Import Routes
 
-import router from './routes/routes';
-app.use('/api', router)
+import router from "./routes/routes";
+app.use("/api", router);
 
+connect(process.env.DB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-// Connect to MongoDB
-connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error(err));
-
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
