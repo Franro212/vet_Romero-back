1f0f18ded16611df3111aa9a8b07a0910c6f64f4
Franco rossi <rossifranco748@gmail.com>
2024-06-03 09:39:51 -0300

    Actualizacion de validaciones

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -8,15 +8,16 @@ import {
 } from "../controllers/admin.js";
 import verifyToken from "../validators/auth.js";
 import {
-  validateUser,
+  validateUserRegister,
   validateUserLogin,
 } from "../validators/userValidator.js";
+
 const router = express.Router();
 
 router.get("/", verifyToken, getAdmin);
 router.post("/login", validateUserLogin, login);
-router.post("/", verifyToken, validateUser, register);
-router.put("/:id", verifyToken, validateUser, updateUser);
+router.post("/", verifyToken, validateUserRegister, register);
+router.put("/:id", verifyToken, validateUserRegister, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -1,70 +1,26 @@
 import Joi from "joi";
 
-const userLoginSchema = Joi.object({
-  email: Joi.string().email().required().messages({
-    "string.email":
-      "El campo de correo electrónico debe ser una dirección de correo válida",
-    "any.required": "El campo de correo electrónico es obligatorio",
-  }),
-  password: Joi.string()
-    .min(8)
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]+$/,
-    )
-    .required()
-    .messages({
-      "string.min": "La contraseña debe tener al menos 8 caracteres",
-      "string.pattern.base":
-        "La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y un carácter especial",
-      "any.required": "El campo de contraseña es obligatorio",
-    }),
-});
-
-export const validateUserLogin = (req, res, next) => {
-  try {
-    const { error } = userLoginSchema.validate(req.body, { abortEarly: false });
-    if (!error) {
-      return next();
-    }
-
-    const validationErrors = error.details.map((err) => err.message);
-    return res.status(400).json({
-      message: "Hubo errores de validación en los datos del usuario",
-      data: null,
-      errors: validationErrors,
-    });
-  } catch (err) {
-    console.error("Error en la validación del usuario:", err);
-    return res.status(500).json({
-      message: "Hubo un error interno en el servidor",
-      data: null,
-      error: true,
-    });
-  }
-};
+// Esquema común para la validación de usuario
 const userSchema = Joi.object({
   email: Joi.string().email().required().messages({
-    "string.email":
-      "El campo de correo electrónico debe ser una dirección de correo válida",
-    "any.required": "El campo de correo electrónico es obligatorio",
+    "string.email": "El correo electrónico debe ser una dirección válida",
+    "any.required": "El correo electrónico es obligatorio",
   }),
   password: Joi.string()
     .min(8)
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]+$/,
-    )
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]+$/)
     .required()
     .messages({
       "string.min": "La contraseña debe tener al menos 8 caracteres",
-      "string.pattern.base":
-        "La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y un carácter especial",
-      "any.required": "El campo de contraseña es obligatorio",
+      "string.pattern.base": "La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y un carácter especial",
+      "any.required": "La contraseña es obligatoria",
     }),
 });
 
-export const validateUser = (req, res, next) => {
+
+const validateUser = (req, res, next, schema) => {
   try {
-    const { error } = userSchema.validate(req.body, { abortEarly: false });
+    const { error } = schema.validate(req.body, { abortEarly: false });
     if (!error) {
       return next();
     }
@@ -84,3 +40,19 @@ export const validateUser = (req, res, next) => {
     });
   }
 };
+
+
+export const validateUserRegister = (req, res, next) => {
+  validateUser(req, res, next, userSchema);
+};
+
+
+export const validateUserLogin = (req, res, next) => {
+  const userLoginSchema = userSchema.keys({
+    password: Joi.string().required().messages({
+      "any.required": "La contraseña es obligatoria",
+    }),
+  });
+  validateUser(req, res, next, userLoginSchema);
+};
+
